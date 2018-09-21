e0616acd618538962f82d5cdfcf75090d8d15f90
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-09-21 20:01:40 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,12 @@
 
 class Book
-  attr_accessors :author :page_count :genre
+  attr_accessors :author, :page_count, :genre
   attr_reader :title
 
   def initialize(title)
     @title = title
   end
-  
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
